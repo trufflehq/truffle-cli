b95aa927af49ef7dceaff5956842ee4d5bb8bb73
Austin Fay <austin@austinfay.com>
2023-09-28 21:48:19 -0600

    refactor: rm old types

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,37 +1,7 @@
-export interface OrgProfileConfig {
-  apiUrl: string;
-  secretKey: string;
-}
-
 export interface CliConfig {
   apiUrl: string;
   userAccessTokens: Record<string, string>;
   currentOrgs: Record<string, string>;
-}
-
-export interface PrivateConfig {
-  secretKey: string;
-  secrets: Record<string, Record<string, string>>;
-}
-
-export interface StepActionRelRuntimeData {
-  query: string;
-  variables: Record<string, unknown>;
-}
-
-export interface StepActionRel {
-  actionPath: string;
-  runtimeData: StepActionRelRuntimeData;
-}
-
-export interface RuntimeData {
-  mode: string;
-  stepActionRels: StepActionRel[];
-}
-
-export interface InstallActionRel {
-  actionPath: string;
-  runtimeData: RuntimeData;
 }
 
 export interface PublicConfig {
@@ -44,7 +14,6 @@ export interface PublicConfig {
     value: string;
     filters: Record<string, { isAll: boolean; rank: number}>
   }[];
-  installActionRel: InstallActionRel;
   functions: {
     slug: string;
     description: string;
